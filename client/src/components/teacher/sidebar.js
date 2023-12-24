40bcc20dfe7d09549a5cd0b361d266b0a194d78b
Sayantan Bose <sayantan485@gmail.com>
2023-12-24 20:17:53 +0530

    Added activate deactivate UI components

---
+++ client/src/components/teacher/sidebar.js
@@ -0,0 +1,135 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { Add, Edit, Feed, Home } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { accessCurrentUser } from '../../actions/authActions';
+
+const drawerWidth = 240;
+
+export default function Sidebar() {
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+    const routes = ['/teacher/landing', '/teacher/questions', '/teacher/questions', '/teacher/papers'];
+    const icons = [<Home />, <Add />, <Edit />, <Feed />]; // Replace with your own icons
+
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
+    const user = accessCurrentUser();
+
+    const drawer = (
+        <div>
+            <Toolbar sx={{margin:"1em 0", flexDirection: "column", alignItems:"flex-start", justifyContent:"left"}}>
+            <Typography variant="body2" component="div">
+                Welcome
+            </Typography>
+            <Typography variant="h6" component="div" >
+                {user.name}
+            </Typography> 
+            </Toolbar>
+            <Divider />
+            <List>
+                {['Home', 'Create a new exam', 'Set questions', 'Your papers'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton component={Link} to={routes[index]}>
+                            <ListItemIcon>{icons[index]}</ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                {['Logout'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar
+                position="fixed"
+                sx={{
+                    width: { sm: `calc(100% - ${drawerWidth}px)` },
+                    ml: { sm: `${drawerWidth}px` },
+                }}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={handleDrawerToggle}
+                        sx={{ mr: 2, display: { sm: 'none' } }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap component="div">
+                        Teacher Dashboard
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Box
+                component="nav"
+                sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+                aria-label="mailbox folders"
+            >
+                {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+                <Drawer
+                    variant="temporary"
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{
+                        keepMounted: true, // Better open performance on mobile.
+                    }}
+                    sx={{
+                        display: { xs: 'block', sm: 'none' },
+                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                    }}
+                >
+                    {drawer}
+                </Drawer>
+                <Drawer
+                    variant="permanent"
+                    sx={{
+                        display: { xs: 'none', sm: 'block' },
+                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                    }}
+                    open
+                >
+                    {drawer}
+                </Drawer>
+            </Box>
+            {/* <Box
+                component="main"
+                sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+            >
+                <Toolbar />
+            </Box> */}
+        </Box>
+    );
+}
